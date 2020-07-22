4de989a9afc0cd5197c27581ac5a217c4cc0f610
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-07-21 20:11:34 -0500

    Updates

---
+++ precache-manifest.93c1919ddde03a77c1dc2942b9b8e0a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db092ae59240b84f915d019f61c6486c",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "ea90cd54dd1a007aa117",
+    "url": "/React-Employee-Directory/static/css/main.e19f3869.chunk.css"
+  },
+  {
+    "revision": "8643219a6476daca3a76",
+    "url": "/React-Employee-Directory/static/js/2.2f01e684.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Employee-Directory/static/js/2.2f01e684.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea90cd54dd1a007aa117",
+    "url": "/React-Employee-Directory/static/js/main.3e4e94b1.chunk.js"
+  },
+  {
+    "revision": "fd74d32e2fdcce7b26d8",
+    "url": "/React-Employee-Directory/static/js/runtime-main.021b6c8f.js"
+  }
+]);
